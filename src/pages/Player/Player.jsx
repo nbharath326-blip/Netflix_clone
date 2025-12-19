79cf4c068d7e587b93981c4dc3d9932b1b1d1ac1
nbharath326-blip <nbharath326@gmail.com>
2025-12-19 16:28:17 +0530

    updates the  Player.jsx

--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -33,7 +33,7 @@ useEffect(() => {
 
   return (
     <div className='player'>
-      <img src={back_arrow_icon} alt="" onClick={() => {navigate(-2)}}/>
+      <img src={back_arrow_icon} alt="" onClick={() => {navigate(-1)}}/>
       <iframe width='90%' height='90%' 
       src={`https://www.youtube.com/embed/${apiData.key}` }
       title='trailer' frameBorder='0' allowFullScreen></iframe>
